2fd74b1cb44246d647ede05bdcca42c87b437c2b
Oleg «Eleidan» Kulyk <helh.saintman@gmail.com>
2016-05-21 00:07:59 +0300

    Implement CLI interface

---
+++ src/cmd/fibo.rs
@@ -0,0 +1,33 @@
+use CliResult;
+use util;
+use rosalind_algo::fibo::fibonacci2;
+
+static USAGE: &'static str = "
+Computes Fibonacci number
+
+Usage:
+    rosalind-algo-solutions fibo <input>
+    rosalind-algo-solutions fibo [options]
+
+Options:
+    -h, --help             Display this message
+";
+
+#[derive(RustcDecodable)]
+struct Args {
+    arg_input: Option<usize>,
+}
+
+pub fn run(argv: &[&str]) -> CliResult<()> {
+    let args: Args = try!(util::get_args(USAGE, argv));
+
+    match args.arg_input {
+        Some(input) => {
+            fibonacci2(input);
+            println!("{}", fibonacci2(input));
+        }
+        None        => unreachable!()
+    }
+
+    Ok(())
+}
---
+++ src/cmd/mod.rs
@@ -0,0 +1,2 @@
+pub mod fibo;
+pub mod bins;
